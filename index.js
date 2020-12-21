b07236ae96e49766735431826e97f6457927c0c5
Frank Zambrano <frankz428@hotmail.com>
2020-12-21 05:33:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let nombre;
+let edad;
+let primera_edad;
+let segunda_edad;
+let calcular_edad;
+let edad_canina;
+
+nombre = 'frank';
+edad = 29;
+
+primera_edad = 2 * 10.5;
+segunda_edad =  edad -2;
+calcular_edad = segunda_edad *4;
+edad_canina = primera_edad + calcular_edad;
+
+console.log ('Hola soy ' +nombre+ ' y mi edad de perro es:');
+console.log(calcular_edad);
+
+console.log (edad);
+
